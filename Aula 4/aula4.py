b39fb8ded5b950a0f5f724214034622d512f3593
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-07-22 16:41:27 -0300

    tipos de dados primitivos e operadores aritméticos

---
+++ Aula 4/aula4.py
@@ -0,0 +1,19 @@
+"""
+tipos de dados
+
+str - string - textos 'assim' "assim'
+int - inteiro 123456 0 -10 -20
+float - real/ponto flutuante - 10.50 1.5 -10.10
+bool - booleano/lógico - true/false 10 == 10
+
++, -, *, /, //, **, %, ()
+"""
+
+print(10 * 10) #multiplicação
+print(10 + 10) #adição
+print(10-5) #subtração
+print(10/2) #divisão
+print("ana"+"bia")
+print(10 // 3) #tras a divisão arredondada
+print( 2 ** 10) # potenciação - 2 elevado a 10
+print(10 % 3) #obtem o resto da divisão
